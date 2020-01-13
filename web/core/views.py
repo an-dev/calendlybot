5db177e5bc697dbf161925fadb587a2e0879f991
andy <andy.idiaghe@gmail.com>
2020-01-13 23:55:38 +0000

    Move to django

--- web/core/views.py
+++ web/core/views.py
@@ -1,23 +1,35 @@
 import logging
 import os
+
 import slack
-from flask import Flask, request
+from django.template.response import TemplateResponse
 
 client_id = os.environ["SLACK_CLIENT_ID"]
 client_secret = os.environ["SLACK_CLIENT_SECRET"]
 oauth_scope = os.environ["SLACK_BOT_SCOPE"]
 
 logger = logging.getLogger(__name__)
-app = Flask(__name__)
 
 
-@app.route("/auth", methods=["GET"])
-def pre_install():
-    return f'<a href="https://slack.com/oauth/authorize?scope={oauth_scope}&client_id={client_id}">Add to Slack</a>'
+def auth(request):
+    return TemplateResponse(request, 'web/index.html',
+                            {'scope': oauth_scope, 'client_id': client_id})
 
 
-@app.route("/post-auth", methods=["GET"])
-def post_install():
+def eligible_user(user):
+    return user['is_bot'] is False and user['id'] != 'USLACKBOT'
+
+
+def send_message_to_users():
+    client = slack.WebClient(token=os.environ["SLACK_USER_TOKEN"])
+    response_users_list = client.users_list()
+    for user in filter(lambda u: eligible_user(u), response_users_list['members']):
+        client.chat_postMessage(
+            channel=user['id'],
+            text=f"Hello {user['real_name']}!")
+
+
+def post_auth(request):
     try:
         # Retrieve the auth code from the request params
         auth_code = request.args['code']
@@ -42,20 +54,3 @@ def post_install():
     except Exception:
         logger.exception("Could not complete auth setup")
         return "Uh oh. Could not setup Auth. Please retry."
-
-
-def eligible_user(user):
-    return user['is_bot'] is False and user['id'] != 'USLACKBOT'
-
-
-def send_message_to_users():
-    client = slack.WebClient(token=os.environ["SLACK_USER_TOKEN"])
-    response_users_list = client.users_list()
-    for user in filter(lambda u: eligible_user(u), response_users_list['members']):
-        client.chat_postMessage(
-            channel=user['id'],
-            text=f"Hello {user['real_name']}!")
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
