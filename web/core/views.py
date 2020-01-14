97eff8b932ab2e0e678d8aa515d6dc9869b303cf
andy <andy.idiaghe@gmail.com>
2020-01-14 11:42:55 +0000

    writing commands

--- web/core/views.py
+++ web/core/views.py
@@ -3,6 +3,8 @@ import os
 
 import slack
 from django.template.response import TemplateResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
 client_id = os.environ["SLACK_CLIENT_ID"]
 client_secret = os.environ["SLACK_CLIENT_SECRET"]
@@ -11,9 +13,10 @@ oauth_scope = os.environ["SLACK_BOT_SCOPE"]
 logger = logging.getLogger(__name__)
 
 
-def auth(request):
+@require_http_methods(["GET"])
+def index(request):
     return TemplateResponse(request, 'web/index.html',
-                            {'scope': oauth_scope, 'client_id': client_id})
+                            {'oauth_scope': oauth_scope, 'client_id': client_id})
 
 
 def eligible_user(user):
@@ -29,16 +32,17 @@ def send_message_to_users():
             text=f"Hello {user['real_name']}!")
 
 
-def post_auth(request):
+@require_http_methods(["GET"])
+def auth(request):
     try:
         # Retrieve the auth code from the request params
-        auth_code = request.args['code']
+        auth_code = request.GET['code']
 
         # An empty string is a valid token for this request
         client = slack.WebClient(token="")
 
         # Request the auth tokens from Slack
-        response = client.oauth_access(
+        response = client.oauth_v2_access(
             client_id=client_id,
             client_secret=client_secret,
             code=auth_code
@@ -46,11 +50,20 @@ def post_auth(request):
 
         # Save the bot token to an environmental variable or to your data store
         # for later use
+        # response doesn't have a bot object
         os.environ["SLACK_USER_TOKEN"] = response['access_token']
         send_message_to_users()
 
         # Don't forget to let the user know that auth has succeeded!
-        return "Auth complete!"
+        msg = "Auth complete!"
     except Exception:
         logger.exception("Could not complete auth setup")
-        return "Uh oh. Could not setup Auth. Please retry."
+        msg = "Uh oh. Could not setup auth. Please retry."
+    return TemplateResponse(request, 'web/auth.html',
+                            {'msg': msg})
+
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def connect(request):
+    import pdb; pdb.set_trace()
