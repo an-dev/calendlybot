bcba4aab9d221235710f32f1113ecf5dd139bffc
andy <andy.idiaghe@gmail.com>
2020-01-18 18:19:41 +0000

    Add slack user name

--- web/core/models.py
+++ web/core/models.py
@@ -8,7 +8,7 @@ class Workspace(models.Model):
 
 class SlackUser(models.Model):
     slack_id = models.CharField(max_length=16, unique=True)
-    # slack_email = models.EmailField(blank=True, null=True)
+    slack_name = models.CharField(max_length=64, blank=True, null=True)
     calendly_email = models.EmailField(unique=True, null=True)
     calendly_authtoken = models.CharField(max_length=64, unique=True, null=True)
     workspace = models.ForeignKey('Workspace', related_name='slackusers', on_delete=models.CASCADE)
