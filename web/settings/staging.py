04f7e22447b5974bf0fe7dcf501ceaa0fc50d78d
Andy <andy.idiaghe@zerodeposit.com>
2020-01-21 13:33:17 +0000

    Add logging config

--- web/settings/staging.py
+++ web/settings/staging.py
@@ -8,3 +8,48 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
 
 SITE_URL = 'https://www.calendlybot.herokuapp.com'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {"()": "django.utils.log.RequireDebugFalse"},
+        "require_debug_true": {"()": "django.utils.log.RequireDebugTrue"},
+    },
+    "formatters": {
+        "django.server": {
+            "()": "django.utils.log.ServerFormatter",
+            "format": "[%(server_time)s] %(message)s",
+        },
+        "console": {
+            "()": "django.utils.log.ServerFormatter",
+            "format": "%(levelname)-8s [%(asctime)s] %(name)s: %(message)s"
+        },
+    },
+    "handlers": {
+        "null": {"level": "DEBUG", "class": "logging.NullHandler"},
+        "console": {
+            "level": "DEBUG",
+            "filters": [],
+            "class": "logging.StreamHandler",
+            "formatter": "console",
+        },
+        "django.server": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "django.server",
+        },
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
+        },
+    },
+    "loggers": {
+        "django": {"handlers": ["console"], "level": "INFO"},
+        "django.server": {"handlers": ["django.server"], "level": "INFO", "propagate": False},
+        "web": {"handlers": ["console"], "level": "DEBUG", "propagate": True},
+        "celery": {"handlers": ["console"], "level": "INFO", "propagate": True},
+        "celery.redirected": {"handlers": ["console"], "level": "DEBUG", "propagate": True},
+    },
+}
