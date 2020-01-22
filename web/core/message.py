23bc89715d44f9e72b043f4977ab415e7807c7cf
Andy <andy.idiaghe@zerodeposit.com>
2020-01-22 16:39:23 +0000

    Add pretty messages

---
+++ web/core/message.py
@@ -0,0 +1,95 @@
+class SlackMarkdownEventCreatedMessage:
+    def __init__(self, name, event_name, event_start_time, invitee_name, invitee_email, invitee_timezone):
+        self.name = name
+        self.event_name = event_name
+        self.event_start_time = event_start_time
+        self.invitee_name = invitee_name
+        self.invitee_email = invitee_email
+        self.invitee_timezone = invitee_timezone
+
+    def get_block(self):
+        return [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"Hi {self.name},\n:calendar:A new event has been *scheduled*."
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "fields": [
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Event Type:*\n{self.event_name}"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Invitee:*\n{self.invitee_name}"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Event Date/Time:*\n{self.event_start_time}"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Invitee Email:*\n*<mailto:{self.invitee_email}|{self.invitee_email}>*"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": " "
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Invitee Time Zone:*\n{self.invitee_timezone}"
+                    }
+                ]
+            }
+        ]
+
+
+class SlackMarkdownEventCanceledMessage:
+    def __init__(self, name, event_name, invitee_name, invitee_email, canceler_name):
+        self.name = name
+        self.event_name = event_name
+        self.invitee_name = invitee_name
+        self.invitee_email = invitee_email
+        self.canceler_name = canceler_name
+
+    def get_block(self):
+        return [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"Hi {self.name},\n:x:The event below has been *canceled*."
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "fields": [
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Event Type:*\n{self.event_name}"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Invitee:*\n{self.invitee_name}"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Canceled by:*\n*{self.canceler_name}*"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Invitee Email:*\n*<mailto:{self.invitee_email}|{self.invitee_email}>*"
+                    }
+                ]
+            }
+        ]
