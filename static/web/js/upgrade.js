ec70c1bdda64b8f1107a2ee8271de4743f323f14
andy <andy.idiaghe@gmail.com>
2020-01-26 23:48:31 +0000

    wip stripe

---
+++ static/web/js/upgrade.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Set your publishable key: remember to change this to your live publishable key in production
+    // See your keys here: https://dashboard.stripe.com/account/apikeys
+    var stripe = Stripe('pk_test_ok6aS8NQwE5J1wpV3Oa7n2Qu00fzNa95ch');
+    var checkoutSessionId = null;
+
+    var createCheckoutSession = function(isBuyingSticker) {
+      fetch("/create-checkout-session", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ false })
+      })
+        .then(function(result) {
+          return result.json();
+        })
+        .then(function(data) {
+          checkoutSessionId = data.checkoutSessionId;
+        });
+    };
+
+    document.querySelector("#submit").addEventListener("click", function(evt) {
+      evt.preventDefault();
+      createCheckoutSession();
+      // Initiate payment
+      stripe
+        .redirectToCheckout({
+          sessionId: checkoutSessionId
+        })
+        .then(function(result) {
+          console.log("error");
+          // If `redirectToCheckout` fails due to a browser or network
+          // error, display the localized error message to your customer
+          // using `result.error.message`.
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+});
