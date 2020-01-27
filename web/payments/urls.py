c41599108e6a7943ff3aa6c49c3c8364cd0eacc7
andy <andy.idiaghe@gmail.com>
2020-01-27 12:54:09 +0000

    wip stripe

--- web/payments/urls.py
+++ web/payments/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from web.payments import views
 
 urlpatterns = [
-    # path("stripe/", include("djstripe.urls", namespace="djstripe")),
-    path('upgrade/', views.upgrade, name='upgrade'),
+    path('', views.upgrade, name='home'),
     path('session-create/', views.session_create, name='session-create'),
+    path('cancel/', views.cancel, name='cancel'),
+    path('success/', views.success, name='success'),
 ]
--- web/settings/base.py
+++ web/settings/base.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.messages',
     'web.core',
-    'djstripe'
 ]
 
 MIDDLEWARE = [
@@ -129,11 +128,5 @@ STATIC_ROOT = os.path.join(STATIC_DIR, 'staticfiles')
 
 SITE_URL = os.getenv('SITE_URL', 'http://localhost:8000')
 
-STRIPE_LIVE_PUBLIC_KEY = os.environ.get("STRIPE_LIVE_PUBLIC_KEY", "<your publishable key>")
-STRIPE_LIVE_SECRET_KEY = os.environ.get("STRIPE_LIVE_SECRET_KEY", "<your secret key>")
-
-STRIPE_TEST_PUBLIC_KEY = os.environ["STRIPE_TEST_PUBLIC_KEY"]
-STRIPE_TEST_SECRET_KEY = os.environ["STRIPE_TEST_SECRET_KEY"]
-
-STRIPE_LIVE_MODE = False  # Change to True in production
-DJSTRIPE_WEBHOOK_SECRET = "whsec_xxx"  # Get it from the section in the Stripe dashboard where you added the webhook endpoint
+STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY", "<your publishable key>")
+STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY", "<your secret key>")
