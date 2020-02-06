267347339ac6e2ea009efae02e766f80488eeed7
Andy <andy.idiaghe@zerodeposit.com>
2020-02-06 12:52:44 +0000

    Fix decorator code

--- web/core/decorators.py
+++ web/core/decorators.py
@@ -56,7 +56,7 @@ def requires_subscription(func):
             # if workspace does not have subscription
             # check if vaild trial
             workspace_slack_id, user_slack_id = signing.loads(signed_value)
-            workspace = Workspace.objects.get(id=workspace_slack_id)
+            workspace = Workspace.objects.get(slack_id=workspace_slack_id)
             trial_end = workspace.created + timedelta(days=7)
             if not workspace.subscription or timezone.now().date() > trial_end:
                 msg = SlackMarkdownUpgradePromptMessage()
