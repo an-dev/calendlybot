d263560c5e0ecff993afb9a5a225c422b100e6b9
Andy <andy.idiaghe@zerodeposit.com>
2020-02-06 12:39:40 +0000

    Add missing url endpoint

--- web/core/urls.py
+++ web/core/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('auth/', views.auth, name='auth'),
     path('connect/', views.connect, name='connect'),
     path('upgrade/', views.upgrade, name='upgrade'),
+    path('help/', views.support, name='support'),
     re_path(r'^handle/(?P<signed_value>[-:\w]+)/$', views.handle, name='handle'),
 ]
