534d17b4683b0a9b229d54b427a753a4c339d892
andy <andy.idiaghe@gmail.com>
2020-02-09 15:47:44 +0000

    Add privacy page

--- web/core/urls.py
+++ web/core/urls.py
@@ -5,6 +5,7 @@ from web.core import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('faq/', views.faq, name='faq'),
+    path('privacy/', views.privacy, name='privacy'),
     path('auth/', views.auth, name='auth'),
     path('connect/', views.connect, name='connect'),
     path('upgrade/', views.upgrade, name='upgrade'),
