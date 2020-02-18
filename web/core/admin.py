69e3389851133876aeb754fd00f447634be7cb6c
andy <andy.idiaghe@gmail.com>
2020-02-18 00:54:15 +0000

    Add django admin

--- web/core/admin.py
+++ web/core/admin.py
@@ -1,3 +1,25 @@
+# Register your models here.
 from django.contrib import admin
 
-# Register your models here.
+from web.core.models import SlackUser, Subscription, Workspace, Webhook
+
+
+class SubscriptionInline(admin.StackedInline):
+    model = Subscription
+
+
+class WorkspaceAdmin(admin.ModelAdmin):
+    inlines = (SubscriptionInline,)
+
+
+class WebhooksInline(admin.StackedInline):
+    extra = 2
+    model = Webhook
+
+
+class SlackUserAdmin(admin.ModelAdmin):
+    inlines = (WebhooksInline,)
+
+
+admin.site.register(Workspace, WorkspaceAdmin)
+admin.site.register(SlackUser, SlackUserAdmin)
