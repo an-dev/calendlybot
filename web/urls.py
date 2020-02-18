1e556ef85eada2c13b18dcda7108fba82c2ab6a0
andy <andy.idiaghe@gmail.com>
2020-02-18 00:54:15 +0000

    Add django admin

--- web/urls.py
+++ web/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('cp/', admin.site.urls),
     path('', include('web.core.urls')),
     path('subscribe/', include('web.payments.urls'), name='subscribe'),
 ]
