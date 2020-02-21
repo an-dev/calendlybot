8c0750afdee6edf88a55d412bee2d2626996a27b
Andy <andy.idiaghe@zerodeposit.com>
2020-02-21 16:02:23 +0000

    SEO basic optimizations

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,29 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.contrib import admin, sitemaps
+from django.contrib.sitemaps.views import sitemap
+from django.urls import path, include, reverse
+
+from web.core.urls import core_map_paths
+
+
+class StaticViewSitemap(sitemaps.Sitemap):
+    priority = 0.5
+    changefreq = 'weekly'
+
+    def items(self):
+        return core_map_paths
+
+    def location(self, item):
+        return reverse(item)
+
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('cp/', admin.site.urls),
     path('', include('web.core.urls')),
     path('subscribe/', include('web.payments.urls'), name='subscribe'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
+
 ]
