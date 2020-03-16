89b4928939ce86b137e8537af0181326e029a232
andy <andy.idiaghe@gmail.com>
2020-03-16 23:15:14 +0000

    wip =

---
+++ web/core/views/static.py
@@ -0,0 +1,29 @@
+import os
+
+from django.shortcuts import render, redirect
+from django.template.response import TemplateResponse
+from django.views.decorators.http import require_http_methods
+
+client_id = os.environ["SLACK_CLIENT_ID"]
+oauth_scope = os.environ["SLACK_BOT_SCOPE"]
+
+
+@require_http_methods(["GET"])
+def index(request):
+    return render(request, 'web/index.html')
+
+
+@require_http_methods(["GET"])
+def install(request):
+    return redirect(
+        f"https://slack.com/oauth/v2/authorize?scope={oauth_scope}&client_id={client_id}")
+
+
+@require_http_methods(["GET"])
+def faq(request):
+    return TemplateResponse(request, 'web/faq.html')
+
+
+@require_http_methods(["GET"])
+def privacy(request):
+    return TemplateResponse(request, 'web/privacy.html')
