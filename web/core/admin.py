79503c715989f706a2cba5732c92c2c94b390d74
andy <andy.idiaghe@gmail.com>
2020-03-18 19:57:52 +0000

    Add details to admin

--- web/core/admin.py
+++ web/core/admin.py
@@ -8,8 +8,17 @@ class SubscriptionInline(admin.StackedInline):
     model = Subscription
 
 
+def tot_users(obj):
+    return obj.slackusers.count()
+
+
+tot_users.short_description = 'Total users'
+
+
 class WorkspaceAdmin(admin.ModelAdmin):
     inlines = (SubscriptionInline,)
+
+    list_display = ('slack_id', 'trial_end', 'subscription', tot_users)
 
 
 class WebhooksInline(admin.StackedInline):
@@ -20,6 +29,8 @@ class WebhooksInline(admin.StackedInline):
 class SlackUserAdmin(admin.ModelAdmin):
     inlines = (WebhooksInline,)
     readonly_fields = ('workspace',)
+
+    list_display = ('slack_email', 'calendly_authtoken', 'workspace', 'is_installer')
 
 
 admin.site.register(Workspace, WorkspaceAdmin)
