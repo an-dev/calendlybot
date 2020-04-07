1e564eae827e30dba7e2286207a4fee3845b14f8
andy <andy.idiaghe@gmail.com>
2020-04-07 12:58:03 +0100

    Email branch

--- web/settings/base.py
+++ web/settings/base.py
@@ -109,6 +109,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
---
+++ web/settings/email.py
@@ -0,0 +1,4 @@
+from .base import *
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_PORT = 1025
