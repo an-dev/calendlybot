4c5983a184679e8ba5a66116bb5e8f7ac3984112
andy <andy.idiaghe@gmail.com>
2020-04-14 23:11:16 +0100

    Add trial end method

--- web/utils/mail.py
+++ web/utils/mail.py
@@ -1,10 +1,14 @@
 import logging
+from datetime import timedelta
 from smtplib import SMTPServerDisconnected
 
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
 from django.template import loader
+from django.utils import timezone
+
+from web.core.models import Workspace, SlackUser
 
 logger = logging.getLogger(__name__)
 
@@ -16,11 +20,14 @@ class SendEmail:
     def subject(self):
         return ""
 
-    def run(self):
-        body = loader.render_to_string('emails/welcome.txt')
-        html = loader.render_to_string('emails/welcome.html')
+    def template_slug(self):
+        return ""
 
-        logger.info(f"Sending welcome email to {self.to_email}")
+    def run(self):
+        body = loader.render_to_string(f'emails/{self.template_slug}.txt')
+        html = loader.render_to_string(f'emails/{self.template_slug}.html')
+
+        logger.info(f"Sending {self.template_slug} email to {self.to_email}")
 
         return send_mail(
             subject=self.subject(),
@@ -35,6 +42,17 @@ class SendWelcomeEmail(SendEmail):
     def subject(self):
         return "Welcome to Calenduck!"
 
+    def template_slug(self):
+        return "welcome"
+
+
+class SendTrialEndEmail(SendEmail):
+    def subject(self):
+        return "Calenduck trial ending soon!"
+
+    def template_slug(self):
+        return "trial-end"
+
 
 @shared_task(autoretry_for=(ValueError, SMTPServerDisconnected))
 def send_welcome_email(user_id):
@@ -45,3 +63,13 @@ def send_welcome_email(user_id):
         SendWelcomeEmail(email).run()
     except Exception:
         logger.exception("Could not send welcome email")
+
+
+def send_trial_end_email():
+    try:
+        emails = SlackUser.objects\
+            .filter(workspace__trial_end=timezone.now().date() + timedelta(days=3))\
+            .values_list('slack_email', flat=True)
+        [SendTrialEndEmail(email).run() for email in emails]
+    except Exception:
+        logger.exception("Could not send trial email")
