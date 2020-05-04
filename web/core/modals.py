099908f1f70aca8b438a4e167478a0846750ba32
andy <andy.idiaghe@gmail.com>
2020-05-04 08:16:19 +0100

    Working connect modal

---
+++ web/core/modals.py
@@ -0,0 +1,124 @@
+class SlackErrorModal:
+    def get_view(self):
+        return {
+            "type": "modal",
+            "title": {
+                "type": "plain_text",
+                "text": self.get_title(),
+            },
+            "close": {
+                "type": "plain_text",
+                "text": "Cancel",
+            },
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": self.get_descr()
+                    }
+                }
+            ]
+        }
+
+
+class SlackDisconnectErrorModal(SlackErrorModal):
+    def get_title(self):
+        return "Disconnect Error"
+
+    def get_descr(self):
+        return "Could not disconnect Calendly account. " \
+               "Please try again or *<mailto:support@calenduck.co|contact support>*"
+
+
+class SlackConnectModal:
+    def get_view(self):
+        return {
+            "type": "modal",
+            "title": {
+                "type": "plain_text",
+                "text": "Connect to Calendly",
+            },
+            "submit": {
+                "type": "plain_text",
+                "text": "Connect",
+            },
+            "close": {
+                "type": "plain_text",
+                "text": "Cancel",
+            },
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*1.* Login into Calendly. At the top of any page, select 'Integrations'.\n"
+                                "*2.* Select Copy Key.\n"
+                                "*3.* Paste in the field down below"
+                    }
+                },
+                {
+                    "type": "image",
+                    "block_id": "image",
+                    "image_url": "https://help.calendly.com/hc/article_attachments/360059408434/Screen_Shot_2020-03-04_at_9.23.54_AM.png",
+                    "alt_text": "Calendly Instructions"
+                },
+                {
+                    "type": "context",
+                    "elements": [
+                        {
+                            "type": "mrkdwn",
+                            "text": "*Calenduck works only with Calendly Premium and Pro accounts. "
+                                    "<https://help.calendly.com/hc/en-us/articles/223195488|Learn more>.*"
+                        }
+                    ]
+                },
+                {
+                    "type": "context",
+                    "elements": [
+                        {
+                            "type": "image",
+                            "image_url": "https://api.slack.com/img/blocks/bkb_template_images/placeholder.png",
+                            "alt_text": "placeholder"
+                        }
+                    ]
+                },
+                {
+                    "type": "input",
+                    "block_id": "block_connect",
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "input_connect"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Add your API Key"
+                    }
+                },
+                {
+                    "type": "context",
+                    "elements": [
+                        {
+                            "type": "mrkdwn",
+                            "text": "We do not share your API Key with any third party and never will. "
+                                    "For more information, see our <https://calenduck.co/privacy/|Privacy Policy>."
+                        }
+                    ]
+                }
+            ]
+
+        }
+
+
+class SlackConnectModalWithError:
+    def __init__(self, error):
+        self.error = error
+
+    def get_view(self):
+        return {
+            "response_action": "errors",
+            "errors": {
+                "block_connect": self.error
+            },
+        }
+
--- web/core/services.py
+++ web/core/services.py
@@ -3,7 +3,7 @@ from calendly import Calendly
 
 from web.core.messages import STATIC_START_MSG, STATIC_FREE_ACCT_MSG, STATIC_HELP_MSG, SlackHomeViewMessage
 from web.core.models import Workspace, SlackUser, Webhook
-from web.utils import has_hooks, remove_hooks, InvalidTokenError
+from web.utils import has_calendly_hooks, remove_calendly_hooks, InvalidTokenError, has_active_hooks
 
 from logging import getLogger
 
@@ -45,24 +45,72 @@ class DisconnectService:
         try:
             su = SlackUser.objects.get(slack_id=self.user_id, workspace__slack_id=self.workspace_id)
             calendly = Calendly(su.calendly_authtoken)
-            if has_hooks(calendly) and remove_hooks(calendly):
-                su.calendly_authtoken = None
-                su.save()
-                value = "Account successfully disconnected.\n" \
-                        "Type `/duck connect your-calendly-token` to associate another Calendly account."
-                Webhook.objects.filter(user=su).delete()
-                return Result.from_success(value)
+
+            if not has_calendly_hooks(calendly):
+                logger.warning(f'Deleting non existent webhooks for user {self.user_id}')
             else:
-                logger.warning(f'Trying to delete non existent webhooks for user {self.user_id}')
-                err = f"Doesn't seem like this account is connected. {STATIC_START_MSG}"
-                return Result.from_failure(err)
+                if remove_calendly_hooks(calendly):
+                    Webhook.objects.filter(user=su).delete()
+                else:
+                    logger.warning(f'Could not delete webhooks for user {self.user_id}. Do this manually!')
+            su.calendly_authtoken = None
+            su.calendly_email = None
+            su.save()
+            value = "Account successfully disconnected. Type `/duck start` to associate another Calendly account."
+            return Result.from_success(value)
         except InvalidTokenError:
             logger.warning(f'User {self.user_id} does not have a valid token')
-            err = STATIC_FREE_ACCT_MSG
+            return Result.from_success(STATIC_FREE_ACCT_MSG)
         except Exception:
             logger.exception('Could not disconnect account from calendly.')
             err = f"Could not disconnect account. {STATIC_HELP_MSG}"
         return Result.from_failure(err)
+
+
+class ConnectService:
+    def __init__(self, user_id, workspace, api_key):
+        self.user_id = user_id
+        self.workspace = workspace
+        self.api_key = api_key
+
+    def run(self):
+        try:
+            su, new_user = SlackUser.objects.get_or_create(slack_id=self.user_id, workspace=self.workspace)
+            if new_user:
+                logger.info(f'New user from existing workspace: {self.user_id}')
+                client = slack.WebClient(token=self.workspace.bot_token)
+                user_info = client.users_info(user=self.user_id)['user']['profile']
+                su.slack_name = user_info.get('first_name', user_info['real_name'])
+                su.slack_email = user_info['email']
+                su.save()
+
+            # atm, we support just one authtoken per user.
+            # Calling this service on the same user effectively overwrites
+            calendly = Calendly(self.api_key)
+            response_from_echo = calendly.echo()
+
+            if 'email' not in response_from_echo:
+                logger.warning(f'Could not find user for api key {self.api_key}')
+                return Result.from_failure("Could not find user in Calendly. Make sure the token is correct.")
+
+            # check if there's an existing working hook for this user
+            # this effectively means that if someone uses another's apiKey
+            # if all its hooks are active they won't be able to setup
+            if has_active_hooks(calendly):
+                logger.error(f'Trying to setup apikey on already setup account for {self.user_id}')
+                return Result.from_failure("Your account is already setup to receive event notifications.")
+
+            su.calendly_authtoken = self.api_key
+            su.calendly_email = response_from_echo['email']
+            su.save()
+            return Result.from_success()
+        except InvalidTokenError:
+            logger.warning(f'User {self.user_id} does not have paid account')
+            msg = STATIC_FREE_ACCT_MSG
+        except Exception:
+            logger.exception('Could not connect to calendly')
+            msg = f"Could not connect to Calendly. {STATIC_HELP_MSG}"
+        return Result.from_failure(msg)
 
 
 class UpdateHomeViewService:
@@ -84,13 +132,15 @@ class UpdateHomeViewService:
 
 class OpenModalService:
     def __init__(self, workspace_id):
-        self.workspace_id = workspace_id
+        workspace = Workspace.objects.get(slack_id=workspace_id)
+        self.client = slack.WebClient(token=workspace.bot_token)
 
-    def run(self, trigger_id, blocks):
+    def run(self, trigger_id, view, push=False):
         try:
-            workspace = Workspace.objects.get(slack_id=self.workspace_id)
-            client = slack.WebClient(token=workspace.bot_token)
-            client.views_open(trigger_id=trigger_id, view=blocks)
+            if push:
+                self.client.views_push(trigger_id=trigger_id, view=view)
+            else:
+                self.client.views_open(trigger_id=trigger_id, view=view)
             return Result.from_success()
         except Exception:
             logger.exception('Could not update or create home view')
