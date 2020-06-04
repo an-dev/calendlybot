7f1cb67a0650f270ab050d2dd71524889636e562
andy <andy.idiaghe@gmail.com>
2020-06-04 12:58:12 +0100

    Almost there

--- web/blog/urls.py
+++ web/blog/urls.py
@@ -1,7 +1,7 @@
-from . import views
+from web.blog import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('', views.PostListView, name='home'),
+    path('<slug:slug>/', views.PostDetailView, name='post_detail'),
 ]
--- web/blog/views.py
+++ web/blog/views.py
@@ -1,12 +1,27 @@
+from django.template.response import TemplateResponse
 from django.views import generic
+from django.views.decorators.http import require_http_methods
+
 from web.blog.models import Post
 
 
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'blog/index.html'
+# class PostList(generic.ListView):
+#     queryset = Post.objects.filter(status=1).order_by('-created_on')
+#     template_name = 'blog/index.html'
 
 
-class PostDetail(generic.DetailView):
-    model = Post
-    template_name = 'blog/post.html'
+@require_http_methods(["GET"])
+def PostListView(request):
+    posts = Post.objects.filter(status=1).order_by('-created_on')
+    return TemplateResponse(request, 'blog/index.html', {'post_list': posts, 'blog_nav': True})
+
+
+# class PostDetailView(generic.DetailView):
+#     model = Post
+#     template_name = 'blog/post.html'
+
+
+@require_http_methods(["GET"])
+def PostDetailView(request, slug):
+    post = Post.objects.get(slug=slug)
+    return TemplateResponse(request, 'blog/post.html', {'post': post, 'blog_nav': True})
