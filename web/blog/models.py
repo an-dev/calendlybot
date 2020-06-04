3b7483dbcc6ba3ae5d08e5e8d7f877847de752d6
andy <andy.idiaghe@gmail.com>
2020-06-04 23:17:32 +0100

    Blog done

--- web/blog/models.py
+++ web/blog/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 from markdownx.models import MarkdownxField
 
 STATUS = (
@@ -12,7 +11,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     description = models.CharField(max_length=150, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    preview = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
     content = MarkdownxField()
     created_on = models.DateTimeField(auto_now_add=True)
