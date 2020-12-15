00efe00de7548c4776d038a578c25b23d5ba2ab4
andy <andy.idiaghe@gmail.com>
2020-12-15 22:17:30 +0000

    Remove

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import url
 from django.contrib import admin, sitemaps
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include, reverse
 from django.views.generic import TemplateView
 
-from web.blog.models import Post
 from web.core.urls import core_map_paths
 
 
@@ -19,37 +17,14 @@ class StaticViewSitemap(sitemaps.Sitemap):
         return reverse(item)
 
 
-class BlogViewSitemap(sitemaps.Sitemap):
-    priority = 1
-    changefreq = 'weekly'
-
-    def items(self):
-        return ('blog',)
-
-    def location(self, item):
-        return reverse(item)
-
-
-class PostViewSitemap(sitemaps.Sitemap):
-    priority = 1
-    changefreq = 'weekly'
-
-    def items(self):
-        return Post.objects.filter(status=1)
-
-
 sitemaps = {
     'static': StaticViewSitemap,
-    'blog': BlogViewSitemap,
-    'post': PostViewSitemap,
 }
 
 urlpatterns = [
     path('cp/', admin.site.urls),
     path('', include('web.core.urls')),
-    path('blog/', include('web.blog.urls')),
     path('subscribe/', include('web.payments.urls'), name='subscribe'),
-    url(r'^markdownx/', include('markdownx.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), ),
 ]
